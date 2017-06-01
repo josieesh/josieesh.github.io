40c4270acfa5a0218a4c8376253fd518561f2279
josieesh <eshkenazi.josephine@gmail.com>
2017-06-01 00:43:51 -0400

    added resume

--- script.js
+++ script.js
@@ -39,3 +39,13 @@ jQuery.fn.loadRepositories = function(username) {
        });
     }
 };
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("resumeOverlay").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("resumeOverlay").style.width = "0%";
+}
